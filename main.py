30819d9f40e213e35eafcd16da21ff0c31ce906f
Junyali <junjia.liu@pm.me>
2024-11-29 10:32:02 +0000

    Implemented Goldbach Conjecture into GUI

--- main.py
+++ main.py
@@ -1,78 +1,128 @@
 
 ## // Imports \\ ##
 import tkinter as tk
+import tkinter.messagebox
 from tkinter import ttk, messagebox
 import timeit
 
-# Sieve of eratosthenes is used to find every prime number up to and including n
-def sieve_of_eratosthenes(n):
-    potential_prime = [True for _ in range(n+1)]
-    primes = []
+class GUI:
+    # Sieve of eratosthenes is used to find every prime number up to and including n
+    def sieve_of_eratosthenes(self, n):
+        self.progressbar.start()
+        self.progress_label.config(text="Getting all natural numbers up to {}".format(n))
 
-    # Check all numbers if prime
-    i = 2
-    while i * i <= n:
-        if potential_prime[i]:
-            for j in range(i * i, n + 1, i):
-                potential_prime[j] = False
-        i += 1
+        potential_prime = [True for _ in range(n + 1)]
+        primes = []
 
-    # Collects all prime numbers into a separate elist
-    for i in range(2, n + 1):
-        if potential_prime[i]:
-            primes.append(i)
+        self.progress_label.config(text="Finding all prime numbers up to {}".format(n))
 
-    return primes
+        # Check all numbers if prime
+        i = 2
+        while i * i <= self.spinbox_value:
+            if potential_prime[i]:
+                for j in range(i * i, n + 1, i):
+                    potential_prime[j] = False
+            i += 1
 
-def trim_prime_two(primes):
-    primes.remove(2)
-    return primes
+        self.progress_label.config(text="Collecting primes".format(n))
 
-def goldbach_strong_conjecture(n):
-    # sum_x and sum_y are two prime numbers of which the sum is n
-    sum_x, sum_y = 0, 0
-    result = 0
-    n = int(n)
+        # Collects all prime numbers into a separate elist
+        for i in range(2, n + 1):
+            if potential_prime[i]:
+                primes.append(i)
 
-    # n must be even
-    if n % 2 == 0:
-        primes = trim_prime_two(sieve_of_eratosthenes(n))
+        self.progress_label.config(text="")
+        self.progressbar.stop()
 
-        while result != n:
-            for prime_i in range(len(primes)):
-                if result == n: break
-                sum_x = primes[prime_i]
-                for prime_j in range(len(primes)):
-                    sum_y = primes[prime_j]
-                    result = sum_x + sum_y
-                    # Checks each combination of primes to see if the sum is equal to n
-                    if result == n: break
+        return primes
 
-    else:
-        print("{number} is not an even natural number; Goldbach's Strong Conjecture cannot be verified with {number}".format(number=n))
+    def trim_prime_two(self, primes):
+        self.progressbar.start()
+        self.progress_label.config(text="Trimming prime 2")
 
-    return sum_x, sum_y
+        primes.remove(2)
 
-def launch_gui():
-    root = tk.Tk()
-    root.title("Goldbach Conjecture")
-    root.geometry("260x500")
+        self.progress_label.config(text="")
+        self.progressbar.stop()
 
-    prompt_label = tk.Label(root, text="Try any even natural number greater than two!", anchor="center", justify="center")
-    prime_spinbox = tk.Spinbox(root, from_=3, to=99999999999999999999, relief="sunken", repeatdelay=500, repeatinterval=100, textvariable=tk.DoubleVar(value=137) ,font=("Arial", 12), bg="lightgrey", fg="black", state="normal", cursor="hand2", bd=3, justify="center", width=10, wrap=True)
-    enter_button = tk.Button(root, text="Let's go!", bd=3, cursor="hand2", overrelief="raised", width=5)
+        return primes
+
+    def goldbach_strong_conjecture(self, primes, n):
+        sums = []
+        # sum_x and sum_y are two prime numbers of which the sum is n
+        sum_x, sum_y = 0, 0
+        result = 0
+
+        self.progressbar.config(mode="determinate")
+        for prime_i in range(len(primes)):
+            if result == n:
+                sums.append("{x} + {y}".format(x=sum_x, y=sum_y))
+            sum_x = primes[prime_i]
+            for prime_j in range(len(primes)):
+                sum_y = primes[prime_j]
+                result = sum_x + sum_y
+                # Checks each combination of primes to see if the sum is equal to n
+                if result == n:
+                    sums.append("{x} + {y}".format(x=sum_x, y=sum_y))
+            self.progress_label.config(text="Checking prime sums [{x} / {y}]".format(x=prime_i * len(primes), y=len(primes) ** 2))
+            self.progressbar["value"] = ((prime_i * len(primes)) / (len(primes) ** 2)) * 100
+
+        return sums
+
+    def start_find(self):
+        n = self.spinbox_value
+        primes = self.sieve_of_eratosthenes(n)
+        primes = self.trim_prime_two(primes)
+
+        sums = self.goldbach_strong_conjecture(primes, n)
+
+        self.results_combo.config(values=sums)
+
+    def changed_spinbox_value(self):
+        self.spinbox_value = self.prime_spinbox.get()
+
+    def clicked_enter_button(self):
+        try:
+            self.spinbox_value = int(self.spinbox_value)
+        except ValueError:
+            tkinter.messagebox.showerror(title="Input Error", message="Non-natural numbers are not accepted", icon=tkinter.messagebox.ERROR)
+        else:
+            if self.spinbox_value <= 2:
+                tkinter.messagebox.showerror(title="Input Error", message="Numbers equal to or less than 2 are not accepted", icon=tkinter.messagebox.ERROR)
+            if self.spinbox_value % 2 != 0:
+                tkinter.messagebox.showerror(title="Input Error", message="Non-even numbers are not accepted", icon=tkinter.messagebox.ERROR)
+
+            self.start_find()
+
+
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Goldbach Conjecture")
+        self.root.geometry("260x230")
+
+        self.prompt_label = tk.Label(self.root, text="Try any even natural number greater than two!", anchor="center", justify="center")
+        self.prime_spinbox = tk.Spinbox(self.root, from_=3, to=99999999999999999999, relief="sunken", repeatdelay=500, repeatinterval=100, textvariable=tk.DoubleVar(value=32368) ,font=("Arial", 12), bg="lightgrey", fg="black", state="normal", cursor="hand2", bd=3, justify="center", width=10, wrap=True, command=self.changed_spinbox_value)
+        self.enter_button = tk.Button(self.root, text="Let's go!", bd=3, cursor="hand2", overrelief="ridge", width=10, command=self.clicked_enter_button)
+        self.progressbar = ttk.Progressbar(self.root, orient="horizontal", mode="determinate", length=250)
+        self.progress_label = tk.Label(self.root, text="", anchor="center", justify="center", wraplength=250)
+        self.results_combo = ttk.Combobox(self.root, state="readonly")
+
+        self.spinbox_value = self.prime_spinbox.get()
 
 
 
-    prompt_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.N)
-    prime_spinbox.grid(row=1, column=0, padx=5, pady=5, sticky=tk.N)
-    enter_button.grid(row=2, column=0, padx=5, pady=5, sticky=tk.N)
+        self.prompt_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.N)
+        self.prime_spinbox.grid(row=1, column=0, padx=5, pady=5, sticky=tk.N)
+        self.enter_button.grid(row=2, column=0, padx=5, pady=5, sticky=tk.N)
+        self.progressbar.grid(row=3, column=0, padx=5, pady=5, sticky=tk.N)
+        self.progress_label.grid(row=4, column=0, columnspan=2, padx=5, pady=5, sticky=tk.N)
+        self.results_combo.grid(row=5, column=0, padx=5, pady=5, sticky=tk.N)
 
-    root.mainloop()
+        self.root.mainloop()
 
 
 if __name__ == '__main__':
-    launch_gui()
+    GUI()
     num = int(input("Please enter an even natural number greater than 2 >> "))
     timer_start = timeit.default_timer()
     x, y = goldbach_strong_conjecture(num)
